11efc1f89110bf8bb37ef52c4087742b44300a92
drzunny <drzunny@hotmail.com>
2015-10-18 19:01:27 +0800

    Add logo

---
+++ test/test-client.py
@@ -0,0 +1,39 @@
+# -*- coding:utf8 -*-
+import requests
+
+def simple_http_get():
+	return None
+
+
+def file_http_get():
+	return None
+
+
+def short_post_request():
+	return None
+
+
+def long_post_request():
+	return None
+
+
+def file_post_request():
+	return None
+
+
+def mix_post_request():
+	pass
+
+
+def json_post_request():
+	pass
+
+
+if __name__ == '__main__':
+	simple_http_get()
+	file_http_get()
+	short_post_request()
+	long_post_request()
+	file_post_request()
+	mix_post_request()
+	json_post_request()
